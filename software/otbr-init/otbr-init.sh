08d9172f10c458d7e4e96b05f82c21aa083f954a
Neal Jackson <nealjack@umich.edu>
2020-05-27 17:06:16 -0700

    Add timeouts and kills for init service
    Also start after all other otbr services

--- software/otbr-init/otbr-init.sh
+++ software/otbr-init/otbr-init.sh
@@ -2,18 +2,25 @@
 ot-ctl reset&
 sleep 1
 killall ot-ctl
-ot-ctl channel 25
+ot-ctl channel 25&
 sleep 1 
-ot-ctl masterkey 00112233445566778899aabbccddeeff
+killall ot-ctl
+ot-ctl masterkey 00112233445566778899aabbccddeeff&
 sleep 1 
-ot-ctl panid 0xface
+killall ot-ctl
+ot-ctl panid 0xface&
 sleep 1 
-ot-ctl extpanid 1111111122222222
+killall ot-ctl
+ot-ctl extpanid 1111111122222222&
 sleep 1 
-ot-ctl ifconfig up
+killall ot-ctl
+ot-ctl ifconfig up&
 sleep 1
-ot-ctl thread start
+killall ot-ctl
+ot-ctl thread start&
 sleep 1
-ot-ctl prefix add fd11:22::/64 pasor
+killall ot-ctl
+ot-ctl prefix add fd11:22::/64 pasor&
 sleep 1
+killall ot-ctl&
 ot-ctl netdataregister
